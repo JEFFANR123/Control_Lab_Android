861c84fc518d19d91d27359c81f1f4d51e18e572
JEFFANR123 <46536453+JEFFANR123@users.noreply.github.com>
2019-11-21 18:25:32 -0500

    Se crea el area de la captura del QR ademas del cuadro de dialogo con 2 opciones para el registro de equipo

--- app/src/main/java/com/example/control_lab_android/ActividadHome.java
+++ app/src/main/java/com/example/control_lab_android/ActividadHome.java
@@ -2,8 +2,15 @@ package com.example.control_lab_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 public class ActividadHome extends AppCompatActivity {
     Bundle recibeDatos;
@@ -17,5 +24,45 @@ public class ActividadHome extends AppCompatActivity {
         recibeDatos = getIntent().getExtras();
         String UserName = recibeDatos.getString("UserName");
         nombreUsuario.setText("User: "+UserName);
+
+        //Creamos nuestro Alert Dialog al iniciar el ActivityHome con el builder
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(ActividadHome.this);
+
+        alertDialogBuilder.setTitle("Registro");
+
+        alertDialogBuilder
+                .setMessage("Desea registrar uso de Equipo Informatico?")
+                .setCancelable(false)
+                .setPositiveButton("Si",new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        //Funcion para escanear el codigo QR para registrar el uso de equipo informatico
+                        // creo el detector qr
+                        BarcodeDetector detector = new BarcodeDetector.Builder(getApplicationContext())
+                                        .setBarcodeFormats(Barcode.QR_CODE)
+                                        .build();
+
+                        // creo la camara fuente
+                        CameraSource camara = new CameraSource
+                                .Builder(getApplicationContext(), detector)
+                                .setRequestedPreviewSize(640, 480)
+                                .build();
+
+
+                    }
+                })
+                .setNegativeButton("No",new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        //En caso de Negativo el Alert se cierra y muestra el menu disponible
+
+                        dialog.cancel();
+                    }
+                }).create().show();
     }
+
+
+
+
+
+
+
 }
