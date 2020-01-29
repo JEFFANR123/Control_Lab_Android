26c5e0815870a16dbce299d7c7da926d09ce2355
JEFFANR123 <46536453+JEFFANR123@users.noreply.github.com>
2020-01-29 10:42:08 -0500

    Se agrego permisos de acceso de camara, se agrego el web service get y post funcionales, login funcional, escanea qr y los separa en los text view por puntos y comas, los servicios estan en php y mysql, agrega el registro despues de la lectura QR a la base de datos.

--- app/src/main/java/com/example/control_lab_android/ActividadHome.java
+++ app/src/main/java/com/example/control_lab_android/ActividadHome.java
@@ -8,43 +8,62 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.blikoon.qrcodescanner.QrCodeActivity;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ActividadHome extends AppCompatActivity {
     private static final int REQUEST_CODE_QR_SCAN = 101;
     Bundle recibeDatos;
-    TextView nombreUsuario;
+    TextView usuario, floor, laboratorio, dispositivo, asignatura;
+    Button guardar;
+    ImageButton capturar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad_home);
-        nombreUsuario = findViewById(R.id.txtUserName);
+        usuario = findViewById(R.id.user);
+        floor = findViewById(R.id.piso);
+        laboratorio = findViewById(R.id.lab);
+        dispositivo = findViewById(R.id.disp);
+        asignatura = findViewById(R.id.asig);
+        guardar = findViewById(R.id.registra);
         recibeDatos = getIntent().getExtras();
         String UserName = recibeDatos.getString("UserName");
-        nombreUsuario.setText("User: " + UserName);
+        usuario.setText("E" + UserName);
+        if (ContextCompat.checkSelfPermission(ActividadHome.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(ActividadHome.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(ActividadHome.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA}, 1000);
+        }
+
+        guardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registrar("http://192.168.100.71:8080/uisrael/insert_post.php");
+            }
+        });
+
 
     }
 
-
-    // La App esta en ejecución
-
-
-    public void inicializarQR(View v) {
-
-
+    public void inicializarQR(View view) {
         //Creamos nuestro Alert Dialog al iniciar el ActivityHome con el builder
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(ActividadHome.this);
-
         alertDialogBuilder.setTitle("Registro");
-
         alertDialogBuilder
                 .setMessage("Desea registrar uso de Equipo Informatico?")
                 .setCancelable(false)
@@ -63,7 +82,6 @@ public class ActividadHome extends AppCompatActivity {
                 dialog.cancel();
             }
         }).create().show();
-
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -80,10 +98,48 @@ public class ActividadHome extends AppCompatActivity {
         if (requestCode == REQUEST_CODE_QR_SCAN) {
             if (data != null) {
                 String lectura = data.getStringExtra("com.blikoon.qrcodescanner.got_qr_scan_relult");
-                Toast.makeText(getApplicationContext(), "Leído: " + lectura, Toast.LENGTH_SHORT).show();
-
+                String cadena= lectura;
+                String[] div = cadena.split(";");
+                String pis=div[0];
+                String lab=div[1];
+                String dis=div[2];
+                String asi=div[3];
+                floor.setText(pis);
+                laboratorio.setText("normal");
+                dispositivo.setText(dis);
+                asignatura.setText(asi);
+                Toast.makeText(getApplicationContext(), "QR Capturado", Toast.LENGTH_SHORT).show();
             }
         }
+    }
+
+    private void registrar(String URL){
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(getApplicationContext(), "Registrado Exitosamente", Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(),error.toString(),Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> parametros=new HashMap<String, String>();
+                parametros.put("cedula",usuario.getText().toString());
+                parametros.put("piso",floor.getText().toString());
+                parametros.put("laboratorio",laboratorio.getText().toString());
+                parametros.put("dispositivo",dispositivo.getText().toString());
+                parametros.put("asignatura",asignatura.getText().toString());
+                return parametros;
+            }
+
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+
     }
 
 }
--- app/src/main/java/com/example/control_lab_android/MainActivity.java
+++ app/src/main/java/com/example/control_lab_android/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.control_lab_android;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,7 +7,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import androidx.appcompat.app.AppCompatActivity;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -17,50 +23,81 @@ public class MainActivity extends AppCompatActivity {
     private Button btnValidar, btnHelp;
     private EditText txtUsuario, txtPassword;
     TextView txtCount;
-    int counter = 3;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnValidar = findViewById(R.id.btnValidar);
-        btnHelp = findViewById(R.id.btnAyuda);
+        btnHelp = findViewById(R.id.btnhelp);
         txtUsuario = findViewById(R.id.txtUsuario);
         txtPassword = findViewById(R.id.txtPassword);
         txtCount = findViewById(R.id.txtCount);
 
-    }
-
-//Creamos el metodo de Validacion Usuario & Password
-    public void validarUP(View v){
-        //Establecemos el metodo setOnClickListener y se crea el onClick
         btnValidar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Validamos primero la excepcion
-                if (!txtUsuario.getText().toString().equals("Santiago")||
-                (!txtPassword.getText().toString().equals("Anrango"))){
-                    counter--;
-                    txtCount.setText(Integer.toString(counter));
-                    Toast.makeText(MainActivity.this,"Acceso Denegado",Toast.LENGTH_SHORT).show();
-                    //El contador inicial en 3 si = a 0 bloquear el boton
-                    if(counter == 0){
-                        btnValidar.setEnabled(false);
-                    }
-                }else if (txtUsuario.getText().toString().equals("Santiago")||
-                        (txtPassword.getText().toString().equals("Anrango"))){
-                    //Creamos el intent para pasar el nombre del usuario de Main al Home
-                    Intent actividadHome = new Intent(MainActivity.this, ActividadHome.class);
-                    actividadHome.putExtra("UserName", txtUsuario.getText().toString());
-                    startActivity(actividadHome);
-                    Toast.makeText(MainActivity.this,"Acceso Concedido",Toast.LENGTH_SHORT).show();
+                getData("http://192.168.100.71:8080/uisrael/search_get.php?cedula=" + txtUsuario.getText() + "");
+                //getData("http://10.12.7.78:8080/uisrael/search_get.php?cedula=123");
+            }
+        });
 
-                }else{
-                    Toast.makeText(MainActivity.this,"Error al Autenticar",Toast.LENGTH_SHORT).show();
+    }
+
+
+    //Creamos el metodo de Validacion Usuario & Password
+    public void getData(String URL) {
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(URL, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                String Usuario;
+                String Password;
+                JSONObject jsonObject = null;
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        jsonObject = response.getJSONObject(i);
+                        Usuario = jsonObject.getString("cedula");
+                        Password = jsonObject.getString("nombre");
+
+                        if (!txtUsuario.getText().toString().equals(Usuario) || !txtPassword.getText().toString().equals(Password)) {
+                            Toast.makeText(getApplicationContext(), "Credenciales Incorrectas", Toast.LENGTH_SHORT).show();
+                            for (int count = 3; count >=0 ; count --){
+                                if (count==0){
+                                    finish();
+                                }
+                            }
+                            //txtCount.setText("Se envia"+Usuario+Password);
+                        } else if (txtUsuario.getText().toString().equals(Usuario) || txtPassword.getText().toString().equals(Password)) {
+                            Acceso();
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Error al Autenticar", Toast.LENGTH_SHORT).show();
+                        }
+
+                    } catch (JSONException e) {
+                        Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
 
                 }
             }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "Error de Conexion", Toast.LENGTH_SHORT).show();
+            }
         });
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(jsonArrayRequest);
+
     }
+
+    public void Acceso() {
+
+        Intent actividadHome = new Intent(MainActivity.this, ActividadHome.class);
+        actividadHome.putExtra("UserName", txtUsuario.getText().toString());
+        Toast.makeText(MainActivity.this, "Acceso Concedido", Toast.LENGTH_SHORT).show();
+        startActivity(actividadHome);
+
+
+    }
+
 }
